2b8bd948da3cc78fc59c881510ec2e010234d8a2
SergeiNaum <sergei.naumencko@yandex.ru>
2024-03-12 22:15:21 +0300

    Preliminary project preparation is complete

--- app/app_config.py
+++ app/app_config.py
@@ -1,7 +1,13 @@
 import os
+from pathlib import Path
+
 from dotenv import load_dotenv
 
-load_dotenv()
+env_path = Path(__file__).resolve().parent.parent / "docker" / "env" / ".env"
+load_dotenv(env_path)
+
+
 
 # DATABASE_URL = os.getenv('DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY')
+
--- app/main.py
+++ app/main.py
@@ -9,18 +9,36 @@ from app.app_config import SECRET_KEY
 app = Flask(__name__)
 app.secret_key = SECRET_KEY
 
+
 @app.route('/')
 def index():
     messages = get_flashed_messages(with_categories=True)
-    return render_template('index.html', messages=messages)
+
+    return render_template('index.html', title='РегРЖД - Главная', messages=messages)
+
+
+@app.route('/reg')
+def reg():
+    messages = get_flashed_messages(with_categories=True)
+
+    return render_template('authTemplates/registration.html', title='РегРЖД - Регистрация', messages=messages)
 
 
 
+@app.route('/login')
+def login():
+    messages = get_flashed_messages(with_categories=True)
+
+    return render_template('authTemplates/login.html', title='РегРЖД - Вход', messages=messages)
 
 
+@app.route('/logout')
+def logout():
+    messages = get_flashed_messages(with_categories=True)
 
+    return render_template('authTemplates/login.html', title='РегРЖД-Вход', messages=messages)
 
 
 
 if __name__ == "__main__":
-    app
+    app.run(debug=True)
